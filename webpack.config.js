8ba53a90204d95bf5c79053805b97d8bd2ff447a
jiangbo <ma021024@126.com>
2019-03-08 20:49:52 +0800

    add config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        index: './lib/index.tsx'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/lib'),
+        library: 'EMUI',
+        libraryTarget: 'umd',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+}
+}
