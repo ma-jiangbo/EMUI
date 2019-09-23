d1569ee2efeb10a403813e5956b02390d2b40101
majiangbo <ma021024@126.com>
2019-09-23 19:12:10 +0800

    export component

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,14 +1,1 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import Component from './icon/icon'
-
-const iconClick: React.MouseEventHandler = event => {
-    console.log(event.target)
-};
-
-ReactDom.render(
-    <div>
-        <Component name='wechat' onClick={iconClick}/>
-    </div>,
-    document.getElementById('root')
-);
+export { default as icon} from './icon/icon'
